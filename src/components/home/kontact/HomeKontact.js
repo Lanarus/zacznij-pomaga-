e1a147936a301fb83bd01f9ed2e3b6a3724e45e2
Bartosz Polanczyk <bpolanczyk@sumologic.com>
2022-04-19 22:53:45 +0200

    Trochę sugestii z ostatnich konsultacji.

--- src/components/home/kontact/HomeKontact.js
+++ src/components/home/kontact/HomeKontact.js
@@ -1,50 +1,54 @@
-import { Decoration } from '../../Decoration/Decoration';
-import { Button } from "../../Button/Button";
+import {Decoration} from '../../Decoration/Decoration';
+import {Button} from "../../Button/Button";
 import './kontact.scss'
-import { Element } from "react-scroll";
+import {Element} from "react-scroll";
 
-import { ROUTES_TO_CLASSNAMES_MAP } from "../../../constants/routes";
-import { KONTACT_ROUTE } from "../../../constants/routeNames";
+import {ROUTES_TO_CLASSNAMES_MAP} from "../../../constants/routes";
+import {KONTACT_ROUTE} from "../../../constants/routeNames";
+import {HomeFooter} from "../footer/HomeFooter";
 
 export const HomeKontact = () => {
-    return ( 
-  <Element 
-     name={ROUTES_TO_CLASSNAMES_MAP[KONTACT_ROUTE]}>
-    <div className="homeKontact">
-     <div className="formContent"> 
-      <div className="decKontact"> 
-        <Decoration text="Skontaktuj się z nami"/>
-      </div>
-        <div className='form'>
-          <div className='input'>
-          <div className='formfield'>
-            <label className='formlabel'>Wpisz swoje imię</label>
-            <input
-              type="text"
-              required
-              />
+  return (
+    <Element
+      name={ROUTES_TO_CLASSNAMES_MAP[KONTACT_ROUTE]}>
+      <div className="homeKontact">
+        <div className="imageWrapper"/>
+        <div className="formContent">
+          <div className="decKontact">
+            <Decoration text="Skontaktuj się z nami"/>
           </div>
-          <div className='formfield'>
-            <label className='formlabel'>Wpisz swój email</label>
-            <input
-              type="text"
-              required
-              />
-          </div>
-          </div>
-          <div className='formfield'>
-            <label className='formlabel'>Wpisz swoją wiadomość</label>
-            <textarea>
+          <div className='form'>
+            <div className='input'>
+              <div className='formfield'>
+                <label className='formlabel'>Wpisz swoje imię</label>
+                <input
+                  type="text"
+                  required
+                />
+              </div>
+              <div className='formfield'>
+                <label className='formlabel'>Wpisz swój email</label>
+                <input
+                  type="text"
+                  required
+                />
+              </div>
+            </div>
+            <div className='formfield'>
+              <label className='formlabel'>Wpisz swoją wiadomość</label>
+              <textarea>
               
             </textarea>
+            </div>
+          </div>
+          <div className='btnKontact'>
+            <Button>Wyślij</Button>
           </div>
         </div>
-        <div className='btnKontact'>
-         <Button>Wyślij</Button>
-        </div>
+        <div className="spacer"/>
+        <HomeFooter/>
       </div>
-   </div> 
-  </Element>
+    </Element>
   );
 }
  
